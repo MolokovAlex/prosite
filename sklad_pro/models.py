0d37160ba1f9fad8788f4825783612dd74810d81
MolokovAlex <milky77@mail.ru>
2026-01-28 01:51:34 +0300

    миграция Групп и загрузка тестовых данных

--- sklad_pro/models.py
+++ sklad_pro/models.py
@@ -7,13 +7,12 @@ from django.db import models
 
 class DBG(models.Model):
     # id_dbg = models.IntegerField(verbose_name='id_DBG', primary_key=True, unique=True)
-    id_dbg = models.IntegerField(verbose_name='id_DBG', unique=True)
-    name = models.CharField(verbose_name='Наименование группы', max_length=50)
-    nameFull = models.CharField(verbose_name='Полное наименование группы', max_length=150)
-    name2 = models.CharField(verbose_name='Наименование группы вар2', max_length=50)
-    # id_parent = models.IntegerField(verbose_name='id_Родительская группа', null=True)# id_parent INTEGER,
+    id_dbg = models.IntegerField(verbose_name='id_DBG', unique=True, blank=True)
+    name = models.CharField(verbose_name='Наименование группы', max_length=50, blank=False)
+    nameFull = models.CharField(verbose_name='Полное наименование группы', max_length=150, blank=True)
+    name2 = models.CharField(verbose_name='Наименование группы вар2', max_length=50, blank=True)
     id_parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, verbose_name='Родительская группа')
-    is_active = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True, blank=False)
     class Meta:
         ordering = ['-id']
         verbose_name = 'Группа'
